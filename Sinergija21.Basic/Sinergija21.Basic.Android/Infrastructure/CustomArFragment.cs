9b835d70e12343886dbf106217cad4128125447e
Miladin Radovic <miladin.radovic@awesome7.com>
2021-11-25 16:49:07 +0100

    Android: Draw a line.

--- Sinergija21.Basic/Sinergija21.Basic.Android/Infrastructure/CustomArFragment.cs
+++ Sinergija21.Basic/Sinergija21.Basic.Android/Infrastructure/CustomArFragment.cs
@@ -4,6 +4,9 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using Google.AR.Sceneform;
+using Google.AR.Sceneform.Math;
+using Google.AR.Sceneform.Rendering;
 using Google.AR.Sceneform.UX;
 using System;
 using System.Collections.Generic;
@@ -14,5 +17,55 @@ namespace Sinergija21.Basic.Droid.Infrastructure
 {
 	internal class CustomArFragment : ArFragment
 	{
+		public override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+
+			SessionInitialization += CustomArFragment_SessionInitialization;
+		}
+
+		private void CustomArFragment_SessionInitialization(object sender, SessionInitializationEventArgs e)
+		{
+			// When session is initialized, we can add 3d models.
+			DrawLine(1);
+
+		}
+		private void DrawLine(float length, float thickness = 0.02f)
+		{
+			Func<Color, Vector3, Node> createNode = (c, v) =>
+			{
+				var node = new Node();
+				node.SetParent(ArSceneView.Scene);
+				MaterialFactory
+					.MakeOpaqueWithColor(Context, c)
+					.ThenAccept(
+						new DelegateConsumer<Material>(m =>
+						{
+							var model = ShapeFactory.MakeCube(v, v.Scaled(0.5f), m);
+							node.Renderable = model;
+						}
+						));
+				return node;
+			};
+			float l = length;
+			float t = thickness;
+			var line = createNode(new Color(255, 0, 0), new Vector3(l, t, t));
+		}
+		/// <summary>
+		/// A way to perform operation in Action delegate where IConsumer interface is needed.
+		/// </summary>
+		public class DelegateConsumer<T> : Java.Lang.Object, Java.Util.Functions.IConsumer
+			where T : Java.Lang.Object
+		{
+			private Action<T> _completed;
+			public DelegateConsumer(Action<T> action)
+			{
+				_completed = action;
+			}
+			public void Accept(Java.Lang.Object t)
+			{
+				_completed(global::Android.Runtime.Extensions.JavaCast<T>(t));
+			}
+		}
 	}
 }
