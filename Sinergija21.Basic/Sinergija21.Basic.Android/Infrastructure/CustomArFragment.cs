cf9379bd3ef07b6a6b4288bb729db4876fa2473e
Miladin Radovic <miladin.radovic@awesome7.com>
2021-11-25 16:53:35 +0100

    Android: Draw coordinate system.

--- Sinergija21.Basic/Sinergija21.Basic.Android/Infrastructure/CustomArFragment.cs
+++ Sinergija21.Basic/Sinergija21.Basic.Android/Infrastructure/CustomArFragment.cs
@@ -27,7 +27,8 @@ namespace Sinergija21.Basic.Droid.Infrastructure
 		private void CustomArFragment_SessionInitialization(object sender, SessionInitializationEventArgs e)
 		{
 			// When session is initialized, we can add 3d models.
-			DrawLine(1);
+			//DrawLine(1);
+			DrawCoordinateSystem(0.5f);
 
 		}
 		private void DrawLine(float length, float thickness = 0.02f)
@@ -50,6 +51,29 @@ namespace Sinergija21.Basic.Droid.Infrastructure
 			float l = length;
 			float t = thickness;
 			var line = createNode(new Color(255, 0, 0), new Vector3(l, t, t));
+		}
+		private void DrawCoordinateSystem(float length, float thickness = 0.02f)
+		{
+			Func<Color, Vector3, Node> createNode = (c, v) =>
+			{
+				var node = new Node();
+				node.SetParent(ArSceneView.Scene);
+				MaterialFactory
+					.MakeOpaqueWithColor(Context, c)
+					.ThenAccept(
+						new DelegateConsumer<Material>(m =>
+						{
+							var model = ShapeFactory.MakeCube(v, v.Scaled(0.5f), m);
+							node.Renderable = model;
+						}
+						));
+				return node;
+			};
+			float l = length;
+			float t = thickness;
+			var xAxis = createNode(new Color(255, 0, 0), new Vector3(l, t, t));
+			var yAxis = createNode(new Color(0, 255, 0), new Vector3(t, l, t));
+			var zAxis = createNode(new Color(0, 0, 255), new Vector3(t, t, l));
 		}
 		/// <summary>
 		/// A way to perform operation in Action delegate where IConsumer interface is needed.
