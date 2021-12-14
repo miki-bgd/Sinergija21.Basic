48780bfa3661206c6ca62ac55354f4a5a672e873
Miladin Radovic <miladin.radovic@awesome7.com>
2021-12-14 14:11:42 +0100

    Simple earth display in front of camera.

--- Sinergija21.Basic/Sinergija21.Basic.Android/Models/AndroidDisplayManager.cs
+++ Sinergija21.Basic/Sinergija21.Basic.Android/Models/AndroidDisplayManager.cs
@@ -18,68 +18,71 @@ using num = System.Numerics;
 
 namespace Sinergija21.Basic.Droid.Models
 {
-	internal class AndroidDisplayManager : IDisplayManager
-	{
-		public event Action Initialized;
+    internal class AndroidDisplayManager : IDisplayManager
+    {
+        public event Action Initialized;
 
-		private ArFragment fragment;
-		private readonly Dictionary<int, Node> nodes = new Dictionary<int, Node>();
-		private int nodeId = 0;
-		public int DrawCoordinateSystem()
-		{
-			var n = CoordinateSystemCreator.Create(0.5f);
-			n.SetParent(fragment.ArSceneView.Scene);
-			int id = nodeId++;
-			nodes.Add(id, n);
-			return id;
-		}
+        private ArFragment fragment;
+        private readonly Dictionary<int, Node> nodes = new Dictionary<int, Node>();
+        private int nodeId = 0;
+        public int DrawCoordinateSystem()
+        {
+            var n = CoordinateSystemCreator.Create(0.5f);
+            n.SetParent(fragment.ArSceneView.Scene);
+            int id = nodeId++;
+            nodes.Add(id, n);
+            return id;
+        }
 
-		public int DrawLine()
-		{
-			var n = LineCreator.Create(new Vector3(0, 0, 0), 0.5f);
-			n.SetParent(fragment.ArSceneView.Scene);
-			int id = nodeId++;
-			nodes.Add(id, n);
-			return id;
-		}
-		public int DrawSphere(num.Vector3 position, float radius)
-		{
-			var p = position;
-			var q = new Vector3(p.X, p.Y, p.Z);
-			var n = SphereCreator.Create(q, radius * 2);
-			n.SetParent(fragment.ArSceneView.Scene);
-			int id = nodeId++;
-			nodes.Add(id, n);
-			return id;
-		}
-		public int LoadModel(string name)
-		{
-			var position = fragment.ArSceneView.Scene.Camera.WorldPosition;
-			var n = ExternalModelCreator.LoadGlb(name, position);
-			n.SetParent(fragment.ArSceneView.Scene);
-			int id = nodeId++;
-			nodes.Add(id, n);
-			return id;
-		}
+        public int DrawLine()
+        {
+            var n = LineCreator.Create(new Vector3(0, 0, 0), 0.5f);
+            n.SetParent(fragment.ArSceneView.Scene);
+            int id = nodeId++;
+            nodes.Add(id, n);
+            return id;
+        }
+        public int DrawSphere(num.Vector3 position, float radius)
+        {
+            var p = position;
+            var q = new Vector3(p.X, p.Y, p.Z);
+            var n = SphereCreator.Create(q, radius * 2);
+            n.SetParent(fragment.ArSceneView.Scene);
+            int id = nodeId++;
+            nodes.Add(id, n);
+            return id;
+        }
+        public int LoadModel(string name)
+        {
+            var position = fragment.ArSceneView.Scene.Camera.WorldPosition;
+            var forward = fragment.ArSceneView.Scene.Camera.Forward;
+            position = new Vector3(position.X, position.Y, position.Z);
+            position = Vector3.Add(position, forward);
+            var n = ExternalModelCreator.LoadGlb(name, position);
+            n.SetParent(fragment.ArSceneView.Scene);
+            int id = nodeId++;
+            nodes.Add(id, n);
+            return id;
+        }
 
 
-		public void SetModelZRotation(int id, float angleDeg)
-		{
-			if (!nodes.ContainsKey(id))
-				throw new NullReferenceException($"Object {id} doesn't exist!");
-			var n = nodes[id];
-			n.WorldRotation = Quaternion.AxisAngle(Vector3.Up(), angleDeg);
-		}
+        public void SetModelZRotation(int id, float angleDeg)
+        {
+            if (!nodes.ContainsKey(id))
+                throw new NullReferenceException($"Object {id} doesn't exist!");
+            var n = nodes[id];
+            n.WorldRotation = Quaternion.AxisAngle(Vector3.Up(), angleDeg);
+        }
 
-		internal Node LoadModelInternal(string name, Vector3 position)
-		{
-			var n = ExternalModelCreator.LoadGlb(name, position);
-			return n;
-		}
-		internal void Initialize(ArFragment fragment)
-		{
-			this.fragment = fragment;
-			Initialized?.Invoke();
-		}
-	}
+        internal Node LoadModelInternal(string name, Vector3 position)
+        {
+            var n = ExternalModelCreator.LoadGlb(name, position);
+            return n;
+        }
+        internal void Initialize(ArFragment fragment)
+        {
+            this.fragment = fragment;
+            Initialized?.Invoke();
+        }
+    }
 }
--- Sinergija21.Basic/Sinergija21.Basic/MainPage.xaml.cs
+++ Sinergija21.Basic/Sinergija21.Basic/MainPage.xaml.cs
@@ -8,37 +8,37 @@ using Xamarin.Forms;
 
 namespace Sinergija21.Basic
 {
-	public partial class MainPage : ContentPage
-	{
-		public MainPage()
-		{
-			InitializeComponent();
-			DI.Display.Initialized += Display_Initialized;
-		}
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            DI.Display.Initialized += Display_Initialized;
+        }
 
-		private void Display_Initialized()
-		{
-			DI.Display.DrawCoordinateSystem();
-			DI.Display.DrawSphere(new System.Numerics.Vector3(0.5f, 0.1f, 0.5f), 0.1f);
-		}
+        private void Display_Initialized()
+        {
+            //DI.Display.DrawCoordinateSystem();
+            DI.Display.DrawSphere(new System.Numerics.Vector3(0.5f, 0.1f, 0.5f), 0.1f);
+        }
 
-		private async void AddLine_Clicked(object sender, EventArgs e)
-		{
-			int lineId = DI.Display.DrawLine();
-			float currentAngle = 0;
-			float angleStepDeg = 9;
-			while(true)
-			{
-				await Task.Delay(50);
-				DI.Display.SetModelZRotation(lineId, currentAngle);
-				currentAngle += angleStepDeg;
-				if (currentAngle > 360)
-					currentAngle -= 360;
-			}
-		}
-		public void AddModel_Clicked(object sender, EventArgs e)
-		{
-			DI.Display.LoadModel("a7logo.glb");
-		}
-	}
+        private async void AddLine_Clicked(object sender, EventArgs e)
+        {
+            int lineId = DI.Display.DrawLine();
+            float currentAngle = 0;
+            float angleStepDeg = 9;
+            while (true)
+            {
+                await Task.Delay(50);
+                DI.Display.SetModelZRotation(lineId, currentAngle);
+                currentAngle += angleStepDeg;
+                if (currentAngle > 360)
+                    currentAngle -= 360;
+            }
+        }
+        public void AddModel_Clicked(object sender, EventArgs e)
+        {
+            DI.Display.LoadModel("earth4.glb");
+        }
+    }
 }
